27259285be3351f91724003db0a46024d20dbfa3
Daniel <d.holguin@protonmail.com>
2023-09-11 21:07:46 -0700

    inital commit

---
+++ psy-lines-backend/src/main.rs
@@ -0,0 +1,25 @@
+use std::{error::Error, net::SocketAddr};
+
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let router = Router::new().route("/health", get(health_check_handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    axum::Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn health_check_handler() -> impl IntoResponse {
+    Html(format!("Healthy"))
+}
