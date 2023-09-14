dd5c238b05d21b0b2240c9c58532e5f71abe5f43
Daniel <d.holguin@protonmail.com>
2023-09-14 00:53:37 -0700

    add material themeing and icon

---
+++ psy-lines-frontend/scripts/build-n-deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+cd "$(dirname "$0")"/..
+
+# Step 1: Build the Angular project (if not already built)
+ng build --configuration production
+
+# Step 2: Build the Docker image
+docker build -t gcr.io/psy-lines/psy-lines-frontend .
+
+# Step 3: Authenticate Docker to GCR
+gcloud auth configure-docker
+
+# Step 4: Push the Docker image to GCR
+docker push gcr.io/psy-lines/psy-lines-frontend
+
+# Step 5: Set the active project
+gcloud config set project psy-lines
+
+# Step 6: Enable necessary APIs (add more APIs if needed)
+gcloud services enable run.googleapis.com
+
+# Step 7: Deploy the container image to Cloud Run
+gcloud run deploy psy-lines-frontend-service --image gcr.io/psy-lines/psy-lines-frontend --platform managed --allow-unauthenticated
+
+
+echo "Deployment completed."
---
+++ psy-lines-frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Feature1Component } from './feature1/feature1.component';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    Feature1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
